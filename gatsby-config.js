67a1f24ad5e000008ab0c27dc13e2e96ec79fd89
Ran Byron <ranbena@gmail.com>
2020-03-22 11:27:35 +0200

    Completed “coming soon”

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: ``,
     author: `@gatsbyjs`,
   },
+  pathPrefix: `/cominghome`,
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -31,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -1,23 +1,49 @@
 import PropTypes from "prop-types"
-import React from "react"
-import { Modal } from "react-bootstrap"
+import React, { useState } from "react"
+import { Modal, Button, Carousel } from "react-bootstrap"
 
-const SubscribeModal = (props) => (
-  <Modal size="lg" centered {...props}>
-    <Modal.Header closeButton>
-      <Modal.Title>
-        <h4>Get Video Release Notifications</h4>
-        <small>Receive updates via email to get the latest on Coming Home.</small>
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <iframe title="Subscribe" src="https://docs.google.com/forms/d/e/1FAIpQLSeUb-y8CNOfEq5_z10NQPuA5FaHLMrkwnzZtbdkIb7ZoBUARQ/viewform?embedded=true" width="100%" height="685" frameborder="0" marginheight="0" marginwidth="0" allowTransparency="false">Loading…</iframe>
-    </Modal.Body>
-  </Modal>
-)
+const SubscribeModal = (props) => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <Modal size="lg" centered {...props} className="soon-modal">
+      <Modal.Header style={{border: 'none'}}>
+        <h1 className="text-center w-100">Coming Home is Coming <em>Soon</em></h1>
+      </Modal.Header>
+      <Modal.Body>
+        <Carousel activeIndex={index} interval={0} wrap={false} controls={false} indicators={false} keyboard={false}>
+          <Carousel.Item key="1">
+            <p>
+              Be the first to know when we launch Coming Home - a free online platform that gives you a practical toolkit to help tell a new story to the world.
+            </p>
+            <p className="text-center" style={{marginTop: 100}}>
+              <Button variant="primary" onClick={() => setIndex(1)}>
+                Click Here to Get Notified
+              </Button>
+            </p>
+          </Carousel.Item>
+          <Carousel.Item key="2">
+            <iframe
+              title="Subscribe"
+              src="https://docs.google.com/forms/d/e/1FAIpQLSeUb-y8CNOfEq5_z10NQPuA5FaHLMrkwnzZtbdkIb7ZoBUARQ/viewform?embedded=true"
+              width="100%"
+              height="380"
+              frameborder="0"
+              allowTransparency="false"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Modal.Body>
+    </Modal>
+  )
+}
 
 SubscribeModal.propTypes = {
-  onHide: PropTypes.func.isRequired,
+  onHide: PropTypes.func,
+}
+
+SubscribeModal.defaultProps = {
+  onHide: null,
 }
 
 export default SubscribeModal
