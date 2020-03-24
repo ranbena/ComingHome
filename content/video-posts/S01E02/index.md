a77824b0c8692b0dacf3b04cc7d622571effa0a7
Ran Byron <ranbena@gmail.com>
2020-03-24 21:55:14 +0200

    New vid! S01E02

--- content/video-posts/S01E02/index.md
+++ content/video-posts/S01E02/index.md
@@ -2,6 +2,6 @@
 date: "2020-03-24T12:00:00.000Z"
 season: 1
 episode: 2
-youtube_id: ''
-duration: 0
+youtube_id: Pf_2q-m4C1Y
+duration: 16
 ---
--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ const IndexPage = ({ data }) => {
         </Row>
       </Container>
       <VideoCarousel edges={edges} season={1} title="Season 1 - The End of The World As We Know It" bgColor="#f7f8f7" />
-      <VideoCarousel edges={edges} season={2} title="Season 2" placeholder="Arriving April 30th" />
+      <VideoCarousel edges={edges} season={2} title="Season 2" placeholder="Arriving March 30th" />
       <section className="patron">
         <Container>
           <Row>
