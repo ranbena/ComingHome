6e7b8955a880714730fa375713c6a045089c0578
Ran Byron <ranbena@gmail.com>
2020-03-26 12:51:25 +0200

    Text updates and fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,15 +44,15 @@ const IndexPage = ({ data }) => {
           <Row>
             <Col sm={12} lg={7}>
               <h2>Become a Patron</h2>
-              <strong>Where will your contribution go?</strong>
-              <br /><br />
+              <br />
               <p>
                 It is the contributions from our community members that allow us to keep our home as a living-learning center for urban ecology and an incubator for climate resilience. Challenging the consumer-alienated thinking structures that are embedded in our minds, we like to separate the giving experience into parts that do not weigh against each other in arbitrary monetary values. We pursue a “deep economy” in the spirit of the deep ecological principle that “the real economy is ecology.” Embodying these values, all content on this website is given freely; your donations allow us to continue giving our time and space for the development of Coming Home, education, healing, and activism.
                 <br /><br />
                 Coronavirus has halted nearly all income-flow to CityTree at this point, as our efforts have been focused on in-person experiences up to now. We recognize this situation as shared for many, asking only those that can to donate so that we can continue our work and keep this platform free for all.
               </p>
+              <br />
               <Button variant="outline-primary" href="https://www.patreon.com/m/CityTree" target="_blank">Become a Patron</Button>
-              </Col>
+            </Col>
           </Row>
         </Container>
       </section>
@@ -65,7 +65,7 @@ const IndexPage = ({ data }) => {
               <br />
               <p>
                 We wish for a world where our bodies, households, and planet are all experienced as home, the sacred vessels for vibrant ecosystems where life thrives fully. We wish for  humans to find their place again in the world with the laws of nature as their guide. 
-                <br />
+                <br /><br />
                 We envision a human culture reconnected with the elements of life, cherishing the soil, respecting the water, celebrating the fire, and honoring the air. In these times of fear, insecurity, and mistrust, we hope for a day when a sense of stability comes not from a system of money, but rather from the abundance of love, connection, and health. Let us return to our role as guardians of life on Earth, let us come home.</p>
               </Col>
           </Row>
