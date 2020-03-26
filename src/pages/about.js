da1fd6cf1145077e6d7303b7a93bc85d05a70506
Ran Byron <ranbena@gmail.com>
2020-03-26 15:12:42 +0200

    More text changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,22 +12,20 @@ const NotFoundPage = () => (
       <h1>About Us</h1>
       <small className="underline">CityTree is a nurturing space for the renewed story of interconnectedness for our society.</small>
       <p>
-        Since 2006, CityTree - an ecological oasis in a fast-paced, concrete-covered city - has been speaking climate truth. <strong>The truth is that our civilization and planet are in a state of breakdown on multiple levels - from the soil which is losing its fertility, to the global economy which is speeding off the rails of runaway growth.</strong>
+        Since 2006, CityTree - an ecological oasis in a fast-paced, concrete-covered city - has been speaking climate truth: our civilization and planet are in a state of breakdown on multiple levels - from the eroding soil, to the global growth economy which is speeding off the rails.
         <br /><br />
-        We see the urgency of climate crisis as a unique opportunity to not simply innovate to sustain our existing systems, but rather to implement solutions that allow for life to thrive on planet Earth. We see it as a call to reunite with the land that holds us and create from a place that recognizes the sacred interconnectedness of our ecosystems.
+        We see the urgency raised by climate crisis as a unique opportunity to not simply innovate to sustain our existing systems, but rather to restore the conditions for life to thrive on planet Earth. We see it as a call to reunite with the land that holds us and create from a place that recognizes the sacred interconnectedness of our ecosystems.
         <br /><br />
-        <strong>Built upon this deep necessity to develop relationships with our natural surroundings, CityTree holds a peaceful space in the heart of Tel Aviv and offers a genuine model of zero-waste, positive impact living in an urban setting.</strong>
+        <strong>Built upon this deep necessity to develop relationships with our natural surroundings, CityTree holds a peaceful space for experimentation and learning in the heart of Tel Aviv and offers a genuine model of zero-waste, positive impact living in an urban setting.</strong>
         <br /><br />
       </p>
       <div className="textbox">
         <h2>Uniting Humanity and Nature</h2>
         <p>
           <br />
-          Our approach at CityTree mirrors the vision we hold for the world: to reimagine humans as fully harmonious participants in a regenerative, life-giving system.
+          Our approach at CityTree mirrors the vision we hold for the world: to reimagine humans as fully harmonious participants in a regenerative, life-giving system. Beyond hosting ecological retreats and sustainability workshops, we design nature-based solutions in the city that merge ancient practices with modern techniques. 
           <br /><br />
-          Beyond hosting ecological retreats and sustainability workshops, we develop nature-based solutions in the city that merge ancient practices with modern techniques.
-          <br /><br />
-          Addressing all levels, from the individual to the political, CityTree works to unite the local cityscape and human society with nature to close systems and build resilience in the face of a changing climate. Our intention is to serve as the experimental grounds to incubate projects that explore food security, circular economy, sustainable energy, waste management solutions, nature rights, and community building.
+          Addressing all levels, from the individual to the political, CityTree works to unite the local cityscape and human society with nature to restore a whole system and build resilience in the face of a changing climate. Our work over the years has supported people who work on experimental projects that explore food security, circular economy, sustainable energy, waste management solutions, nature rights, and community building.
           <br /><br />
         </p>
         <div>
